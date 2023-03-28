a6277e16b1293b639f9b7eabb02165ef499da902
Alexandro Tapia <zpaceway@gmail.com>
2023-03-28 13:07:37 -0500

    add api key checker

--- chat_bot/__init__.py
+++ chat_bot/__init__.py
@@ -1,26 +1,62 @@
-from os import getenv
+from os import getenv, environ
+from typing import Optional, List
 from llama_index import SimpleDirectoryReader, GPTSimpleVectorIndex
 from django.conf import settings
 from pathlib import Path
+import json
 
-OPENAI_API_KEY = getenv("OPENAI_API_KEY")
+OPENAI_API_KEYS = json.loads(getenv("OPENAI_API_KEYS"))
+
+
+def set_openai_api_key(api_key: str):
+    environ["OPENAI_API_KEY"] = api_key
 
 
 class ChatBotIndex:
     instance: GPTSimpleVectorIndex = None
+    selected_index_api_key = 0
 
     def __init__(self) -> None:
+        openai_api_key = OPENAI_API_KEYS[self.selected_index_api_key]
+        set_openai_api_key(openai_api_key)
         self.train_model()
 
     def refresh_model(self):
         self.train_model()
 
-    def train_model(self):
-        documents = SimpleDirectoryReader(
-            Path.joinpath(settings.BASE_DIR, settings.MEDIA_ROOT)
-        ).load_data()
+    def get_next_possible_api_key(self) -> str:
+        self.selected_index_api_key = (
+            self.selected_index_api_key + 1
+            if self.selected_index_api_key + 1 < len(OPENAI_API_KEYS)
+            else 0
+        )
+        openai_api_key = OPENAI_API_KEYS[self.selected_index_api_key]
 
-        self.instance = GPTSimpleVectorIndex(documents)
+        return openai_api_key
+
+    def train_model(self, tried: Optional[List[str]] = None):
+        openai_api_key = OPENAI_API_KEYS[self.selected_index_api_key]
+        tried = tried or []
+        print(
+            f"trying api key {OPENAI_API_KEYS[self.selected_index_api_key]} , {tried}"
+        )
+        try:
+            tried.append(openai_api_key)
+
+            documents = SimpleDirectoryReader(
+                Path.joinpath(settings.BASE_DIR, settings.MEDIA_ROOT)
+            ).load_data()
+
+            self.instance = GPTSimpleVectorIndex(documents)
+
+        except:
+            openai_api_key = self.get_next_possible_api_key()
+            set_openai_api_key(openai_api_key)
+
+            if not openai_api_key in tried:
+                return self.train_model(tried)
+
+            return
 
 
 chat_bot_index = ChatBotIndex()
